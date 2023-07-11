6c066000aeb9e647762187aa727d160def8310a7
basudev.25sahu@gmail.com <basudev.25sahu@gmail.com>
2023-07-11 14:24:33 +0530

    One more call

--- Program.cs
+++ Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
+    public class Emp
+    {
+        public string FullName(string fname, string lname)
+        {
+            return fname + "" + lname;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Emp emp = new Emp();
+            Console.WriteLine(emp.FullName("Sam", "Dicosta"));
+            Console.WriteLine(emp.FullName(lname : "Dicosta", fname : "Sam"));
+            Console.WriteLine(emp.FullName(lname: "Sharma", fname: "Vivek"));
+            Console.WriteLine(emp.FullName(lname: "Sharma", fname: "Vivek"));
+            Console.ReadKey();
         }
     }
 }
